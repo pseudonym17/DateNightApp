02506abb06936fe5a027e052eb6d7248fb3e41d2
Brandon Densmore <densmoreb1@icloud.com>
2021-11-01 09:47:04 -0600

    added some to login

--- app/src/main/java/com/example/datenightapp/loginPage.kt
+++ app/src/main/java/com/example/datenightapp/loginPage.kt
@@ -1,9 +1,15 @@
 package com.example.datenightapp
 
+import android.content.ContentValues.TAG
 import android.content.Intent
+import android.media.MediaDataSource
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 
 class loginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +19,24 @@ class loginPage : AppCompatActivity() {
         supportActionBar?.hide()
 
         val button = findViewById<Button>(R.id.loginbtn)
+        val db = FirebaseFirestore.getInstance()
 
         button.setOnClickListener {
 //            write the firebase login function here
+
+            val user_name = findViewById<EditText>(R.id.username)
+            val password = findViewById<EditText>(R.id.password)
+
+            db.collection("users")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        Toast.makeText(this, "${document.id} => ${document.data["password"]}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents.", exception)
+                }
             val intent = Intent(this, HomePage::class.java)
             startActivity(intent)
         }
