a83b3aaa922d0071d2f8767367ada4a46515bad4
Cailey <disneygirlcm@gmail.com>
2021-11-29 11:16:56 -0700

    Changed files to Relative layout, added add activity.

--- app/src/main/java/com/example/datenightapp/AddActivity.kt
+++ app/src/main/java/com/example/datenightapp/AddActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,13 +15,28 @@ class AddActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
+        val db = FirebaseFirestore.getInstance()
         val addBtn = findViewById<Button>(R.id.addBtn)
         addBtn.setOnClickListener{
             val title = findViewById<EditText>(R.id.title)
             val description = findViewById<EditText>(R.id.description)
             val address = findViewById<EditText>(R.id.address)
             val price = findViewById<EditText>(R.id.price)
+            val url = findViewById<EditText>(R.id.url)
+
             // Here add an activity to the database
+            val activity: MutableMap<String, Any> = HashMap()
+            activity["name"] = title
+            activity["description"] = description
+            activity["address"] = address
+            activity["price"] = price
+            activity["image_url"] = url
+
+            // Needs document number
+            db.collection("activities").document()
+                .set(activity)
+                .addOnSuccessListener { Toast.makeText(this, "Successfully Added Activity", Toast.LENGTH_SHORT).show() }
+                .addOnFailureListener { Toast.makeText(this, "Connection Error", Toast.LENGTH_SHORT).show() }
         }
 
         val homeBtn = findViewById<Button>(R.id.homeBtn)
